67dd10182d9c042a7471ba7c1995d81c19a678f5
Cody Fackrell <codyfackrell56@gmail.com>
2025-11-15 07:48:48 -0700

    updating oauth

--- server/oauth.js
+++ server/oauth.js
@@ -1,28 +1,43 @@
-async function run() {
-  const client = new AuthorizationCode(config);
+const config = {
+  client: {
+    id: "<client-id>",
+    secret: "<client-secret>",
+  },
+  auth: {
+    tokenHost: "https://garage61.net",
+    tokenPath: "/api/oauth/token",
+    authorizeHost: "https://garage61.net",
+    authorizePath: "/app/account/oauth",
+  },
+};
 
-  const authorizationUri = client.authorizeURL({
-    redirect_uri: "http://localhost:3000/auth/callback",
-    scope: "<scope>",
-    state: "<state>",
+const { AuthorizationCode } = require("simple-oauth2");
 
-    customParam: "foo", // non-standard oauth params may be passed as well
-  });
+const client = new AuthorizationCode(config);
 
-  // Redirect example using Express (see http://expressjs.com/api.html#res.redirect)
-  res.redirect(authorizationUri);
+const state = crypto.randomUUID();
 
-  const tokenParams = {
-    code: "<code>",
-    redirect_uri: "http://localhost:5000/auth/callback",
-    scope: "<scope>",
-  };
+const authorizationUri = client.authorizeURL({
+  redirect_uri: "http://localhost:5000/auth/callback",
+  scope: "driving_data datapacks_subscriptions",
+  state: state,
+});
 
-  try {
-    const accessToken = await client.getToken(tokenParams);
-  } catch (error) {
-    console.log("Access Token Error", error.message);
-  }
+// *********************
+
+// Redirect example using Express (see http://expressjs.com/api.html#res.redirect)
+res.redirect(authorizationUri);
+
+const tokenParams = {
+  code: "<code>",
+  redirect_uri: "http://localhost:5000/auth/callback",
+  scope: "driving_data datapacks_subscriptions", // Optional string or array including a subset of the original client scopes to request
+};
+
+try {
+  const accessToken = await client.getToken(tokenParams);
+} catch (error) {
+  console.log("Access Token Error", error.message);
 }
 
 run();
