0a9f2d9bee9a88d51bd3eec7f44042a68b5f1ca6
Cody Fackrell <codyfackrell56@gmail.com>
2025-11-19 14:19:05 -0700

    update gemini api key

--- server/gemini.js
+++ server/gemini.js
@@ -4,18 +4,7 @@ dotenv.config();
 
 import { GoogleGenAI } from "@google/genai";
 
-// The client gets the API key from the environment variable `GEMINI_API_KEY`.
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
-
-async function main() {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: "Explain how skidse works in a few words",
-  });
-  console.log(response.text);
-}
-
-main();
 
 // possible code to input
 async function main() {
