838b6074dd30def06351871c444684253dfc61a9
Cody Fackrell <codyfackrell56@gmail.com>
2025-11-25 22:15:23 -0700

    added analysis page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./css/App.css";
 import Home from "./components/Home";
 import Begin from "./components/Begin";
+import Analyze from "./components/Analyze";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path={"/"} element={<Home />} />
         <Route path={"/begin"} element={<Begin />} />
+        <Route path={"/analyze"} element={<Analyze />} />
         {/* <Route path={"/signup"} element={<Signup />} /> */}
         {/* <Route path={"/log"} element={<Log />} /> */}
       </Routes>
---
+++ src/components/Analyze.jsx
@@ -0,0 +1,9 @@
+function Analyze() {
+  return (
+    <>
+      <h1>Begin Analyzing</h1>
+    </>
+  );
+}
+
+export default Analyze;
