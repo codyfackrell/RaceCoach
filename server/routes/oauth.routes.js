5704724dbf72f4783a50d8e350053aba3af56d72
Cody Fackrell <codyfackrell56@gmail.com>
2026-01-19 20:21:12 -0700

    added controllers

--- server/routes/oauth.routes.js
+++ server/routes/oauth.routes.js
@@ -1,64 +1,9 @@
 import express from "express";
-import { oauthClient } from "../config/oauth.config.js";
-import { generateUUID, generateCodeChallenge } from "../utils/pkce.js";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const router = express.Router();
 
 // OAuth2 Routes
-router.get("/auth", (req, res) => {
-  const codeVerifier = generateUUID();
-  const codeChallenge = generateCodeChallenge(codeVerifier);
-
-  req.session.pkceVerifier = codeVerifier;
-
-  const state = generateUUID();
-  req.session.oauthState = state;
-
-  const authorizationUri = oauthClient.authorizeURL({
-    redirect_uri: "http://localhost:5000/auth/callback",
-    scope: "driving_data",
-    state: state,
-    code_challenge: codeChallenge,
-    code_challenge_method: "S256",
-  });
-
-  res.redirect(authorizationUri);
-});
-
-router.get("/auth/callback", async (req, res) => {
-  const { code, state } = req.query;
-
-  if (state != req.session.oauthState) {
-    return res.status(403).send("Invalid state");
-  }
-
-  try {
-    const token = await oauthClient.getToken({
-      code,
-      redirect_uri: "http://localhost:5000/auth/callback",
-      code_verifier: req.session.pkceVerifier,
-    });
-
-    res.cookie("access_token", token.access_token, {
-      maxAge: 1000 * 60 * 60, // one hour
-      httpOnly: true,
-      secure: true,
-      sameSite: "Lax",
-    });
-    res.cookie("refresh_token", token.refresh_token, {
-      maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "Lax",
-    });
-
-    return res.redirect("/"); // Need to update with correct route
-  } catch (error) {
-    return res.redirect("/auth/error"); // Need to update with correct route
-  }
-});
+router.get("/auth");
+router.get("/auth/callback");
 
 export default router;
