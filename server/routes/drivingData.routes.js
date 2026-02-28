b27aa228a1fd79ae63a407572fc186f325525683
Personal <personal@1179-cody-fackrell.taile9af35.ts.net>
2026-02-27 20:25:24 -0700

    updated driving controllers and routes

--- server/routes/drivingData.routes.js
+++ server/routes/drivingData.routes.js
@@ -1,2 +1,12 @@
 import express from "express";
 import drivingDataControllers from "../controllers/drivingData.controllers.js";
+
+const { getPersonalData, personalDrivingData } = drivingDataControllers;
+
+const router = express.Router();
+
+// Garage61 Driving Data - Personal Data
+router.get("/api/me", getPersonalData);
+router.get("/api/driving-data", personalDrivingData);
+
+export default router;
--- server/routes/oauth.routes.js
+++ server/routes/oauth.routes.js
@@ -1,16 +1,12 @@
 import express from "express";
 import oauthControllers from "../controllers/oauth.controllers.js";
 
-const { getAuthUri, getAuthToken, getPersonalData, personalDrivingData } =
-  oauthControllers;
+const { getAuthUri, getAuthToken } = oauthControllers;
 
 const router = express.Router();
 
 // OAuth2 Routes
 router.get("/auth", getAuthUri);
 router.get("/auth/callback", getAuthToken);
-
-router.get("/api/me", getPersonalData);
-router.get("/api/driving-data", personalDrivingData);
 
 export default router;
